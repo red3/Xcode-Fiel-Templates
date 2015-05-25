72a169018f55ade23cd641fcc162f74c301bd04b
herui <heruicross@163.com>
2015-05-25 13:08:53 +0800

    add base ViewController file

---
+++ ___FILEBASENAME___.m
@@ -0,0 +1,38 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - DeInit
+
+- (void)dealloc {
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Life Cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+#pragma mark - Delegate
+
+#pragma mark - Event Response
+
+#pragma mark - Setter && Getter
+
+@end
